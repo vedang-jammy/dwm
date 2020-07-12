df1d67e5edfdb8095eeea8eac0971699225c85d4
vedang-jammy <upadhyevedang18@gmail.com>
2020-07-12 14:16:09 +0000

    minor changes in config

--- dwm/config.h
+++ dwm/config.h
@@ -48,8 +48,8 @@ static const Rule rules[] = {
 	/* class    instance      title       	 tags mask    isfloating   isterminal  noswallow  monitor */
 	{ "Gimp",     NULL,       NULL,       	    1 << 8,       0,           0,         0,        -1 },
 	{ "terminator", NULL,       NULL,       	    0,            0,           1,         0,        -1 },
-	{ "firefox",  NULL,       NULL,       	    7 << 0,       0,           0,         0,        -1 },
-	{ "spotify",  NULL,       NULL,       	    5 << 0,       0,           0,         0,        -1 },
+	{ "firefox",  NULL,       NULL,       	    1 << 6,       0,           0,         0,        -1 },
+	{ "Spotify",  NULL,       NULL,       	    1 << 4,       0,           0,         0,        -1 },
 	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
 	{ NULL,      "spterm",    NULL,       	    SPTAG(0),     1,           1,         0,        -1 },
 	{ NULL,      "spcalc",    NULL,       	    SPTAG(1),     1,           1,         0,        -1 },
